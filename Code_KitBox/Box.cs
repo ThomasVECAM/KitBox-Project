65997d04e60cfa6d24c0b8b4e3303693402e9760
ThomasVECAM <17030@ecam.be>
2020-02-27 21:27:37 +0100

    Adding Box template

--- Code_KitBox/Box.cs
+++ Code_KitBox/Box.cs
@@ -6,8 +6,37 @@ using System.Threading.Tasks;
 
 namespace Code_KitBox
 {
+
+
     class Box
     {
-        //hello
+        private List<Component> componentList;
+        private int height;
+
+        public Box()
+        {
+            componentList = new List<Component>();
+            height = 0; //by default
+        }
+
+        public int Name 
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public void AddComponent(Component component)
+        {
+            componentList.Add(component);
+        }
+
+        public void RemoveComponent(Component component)
+        {
+            componentList.Remove(component);
+        }
+        public int GetPrice()
+        {
+            return 0;
+        }
     }
 }
