42f6690d4843e64df72dfbeaada6691049c7f335
Landry TAYMANS <17046@ecam.be>
2020-02-27 15:36:45 +0100

    Add id variable in Component classe

--- Code_KitBox/Component.cs
+++ Code_KitBox/Component.cs
@@ -9,6 +9,7 @@ namespace Code_KitBox
     abstract class Component
     {
         private int price;
+        public int id;
         public int height;
         public int width;
         public int depth;
