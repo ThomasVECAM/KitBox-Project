78bbaa435a164f2b975f5d7ee71191fd1747214d
ThomasVECAM <17030@ecam.be>
2020-02-27 14:25:40 +0100

    Add Order and Furniture classes

---
+++ Code_KitBox/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_KitBox
+{
+    class Order
+    {
+        private int id;
+        private List<Furniture> furnitureList;
+
+
+        public Order()
+        {
+            id = 1;
+            this.furnitureList = new List<Furniture>();
+        }
+        
+        public void AddFurniture(Furniture furniture)
+        {
+            furnitureList.Add(furniture);
+        }
+
+        public void  RemoveFurniture(Furniture furniture)
+        {
+            furnitureList.Remove(furniture);
+        }
+
+        public void GetPrice()
+        {
+            //to do
+        }
+    }
+}
