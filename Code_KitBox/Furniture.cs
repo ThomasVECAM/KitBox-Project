89f3984f187acdb166e7fabe17be51683ef1e7a2
ThomasVECAM <17030@ecam.be>
2020-02-27 21:08:07 +0100

    Furniture class template

--- Code_KitBox/Furniture.cs
+++ Code_KitBox/Furniture.cs
@@ -8,10 +8,32 @@ namespace Code_KitBox
 {
     class Furniture
     {
-        //test
-        public Furniture()
+        private string name;
+        private List<Box> boxList;
+        private List<Corner> cornerList;
+        private int length, width;
+
+        public Furniture(string name, int length, int width)
         {
-            
+            this.name = name;
+            this.length = length;
+            this.width = width;
+            this.boxList = new List<Box>();
+            this.cornerList = new List<Corner>();
+        }
+
+        public void AddBox(Box box)
+        {
+            boxList.Add(box);
+        }
+        public void Remove(Box box)
+        {
+            boxList.Remove(box);
+        }
+        public int GetPrice()
+        {
+            //to do
+            return 0;
         }
     }
 }
