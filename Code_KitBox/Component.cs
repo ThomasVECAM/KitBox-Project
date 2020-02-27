7352ebb18492bd58aabb69a607b07e0d404549c9
Landry TAYMANS <17046@ecam.be>
2020-02-27 15:16:18 +0100

    Add Component classe

---
+++ Code_KitBox/Component.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_KitBox
+{
+    abstract class Component
+    {
+        private int price;
+        public int height;
+        public int width;
+        public int depth;
+
+        public int GetPrice()
+        {
+            // To Do
+            return 0;
+        }
+    }
+}
