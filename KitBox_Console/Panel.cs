e0b1ea4baa0da9e584bbd08119f9765857ac1c09
LandryECAM <17046@student.ecam.be>
2020-03-08 15:50:58 +0100

    Write the body of Panel

--- KitBox_Console/Panel.cs
+++ KitBox_Console/Panel.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace KitBox_Console
 {
-    class Panel
+    class Panel : Component
     {
+        private string orientation, color;
+
+        public Panel(string id, int height, int width, int depth,
+        int price, string color) : base(id, height, width, depth, price)
+        {
+            this.color = color;
+            if (height == 0)
+            {
+                this.orientation = "horizontal";
+            }
+            else if (depth == 0)
+            {
+                this.orientation = "back";
+            }
+            else
+            {
+                this.orientation = "side";
+            }
+        }
+        public string GetOrientation
+        {
+            get { return this.orientation; }
+        }
+        public string GetColor
+        {
+            get { return this.color; }
+        }
     }
 }
