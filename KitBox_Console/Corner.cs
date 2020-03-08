04ad1c80fa7798180ac08f17d7670be1e49e0d75
LandryECAM <17046@student.ecam.be>
2020-03-08 15:50:43 +0100

    Write the body of Corner

--- KitBox_Console/Corner.cs
+++ KitBox_Console/Corner.cs
@@ -8,5 +8,19 @@ namespace KitBox_Console
 {
     class Corner
     {
+        private string id, color;
+        private int height, price;
+
+        public Corner(string id, int height, int price, string color)
+        {
+            this.id = id;
+            this.height = height;
+            this.price = price;
+            this.color = color;
+        }
+        public int GetPrice
+        {
+            get { return this.price; }
+        }
     }
 }
