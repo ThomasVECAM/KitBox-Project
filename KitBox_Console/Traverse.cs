81cdd1340af265d42c5d3fcfbafdc410f9cd46f7
LandryECAM <17046@student.ecam.be>
2020-03-08 15:51:06 +0100

    Write the body of Traverse

--- KitBox_Console/Traverse.cs
+++ KitBox_Console/Traverse.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace KitBox_Console
 {
-    class Traverse
+    class Traverse : Component
     {
+        private string orientation;
+
+        public Traverse(string id, int height, int width, int depth,
+        int price) : base(id, height, width, depth, price)
+        {
+            if (id.Contains("TRR"))
+            {
+                this.orientation = "back";
+            }
+            else if (id.Contains("TRF"))
+            {
+                this.orientation = "front";
+            }
+            else
+            {
+                this.orientation = "side";
+            }
+        }
+
+        public string GetOrientation
+        {
+            get { return this.orientation; }
+        }
     }
 }
