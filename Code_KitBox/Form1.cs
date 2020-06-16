393df86bdfedb0f8184117877b6cbc34f15d5b03
ThomasVECAM <17030@ecam.be>
2020-06-16 23:06:05 +0200

    Database transfer

--- Code_KitBox/Form1.cs
+++ Code_KitBox/Form1.cs
@@ -11,7 +11,7 @@ namespace Interface_5
         {
             Globals.dataBaseComponents = new DataBaseComponents();
 
-            MySqlConnection db = new MySqlConnection("SERVER=db4free.net;PORT=3306;DATABASE=groupe5;UID=groupe5;PWD=4c66dfc7; old guids=true");
+            MySqlConnection db = new MySqlConnection("SERVER=mysql-projetkitbox.alwaysdata.net;PORT=3306;DATABASE=projetkitbox_db;UID=207353_thomas	;PWD=Y@sqydv6Yp798Xy; old guids=true");
             bool connected = false;
             while (!connected)
                 try
---
+++ Factures/26_Test NewDB.md
@@ -0,0 +1,17 @@
+### Kitbox Project magasin
+---
+||||
+|-|-|-|
+|**ID Client :** 31|**Nom :** Test NewDB||
+|**Adresse mail :** newdb@test.be|**N° téléphone :** 898999998||
+|**Adresse :** Avenue Saint Hubert|**Ville :** Wezembeek Oppem|**Code postal :** 1234|
+# Facture de la commande n° 26
+||*Quantité*|*Prix (€)*|
+| -|:-:| -:|
+|**Cuisine **|3|240,516|
+|Cornière Noir|4|1,26|
+|Box 1|1|78,912|
+|||
+||*Total (HTVA)*|*198,774*|
+||*TVA (21%)*|*41,742*|
+||**Prix total**|**240,516**|
